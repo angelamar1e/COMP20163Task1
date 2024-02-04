15432d1a2cdc36e0f8ed9bf34d4a3a35da02c277
angela <angela.delapena06@gmail.com>
2024-02-04 23:16:07 +0800

    added images, basic form template

---
+++ form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" id="flower_survey_form" class="flower_survey_form">
+        <input type="text" class="form_fields">
+        <select name="flower_types" id="flower_types">
+            <option value=""></option>
+            <option value=""></option>
+        </select>
+        <input type="text" class="form_fields">
+        <input type="radio" class="form_fields">
+    </form>
+</body>
+</html>
